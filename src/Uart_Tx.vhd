d2e4dd6853a547316a8f837ca0aa7715bea96db2
Varun Posimsetty <VarunSrinivas.Posimsetty@infineon.com>
2024-12-16 09:57:28 +0100

    1 clock cycle delay leading to error

--- src/Uart_Tx.vhd
+++ src/Uart_Tx.vhd
@@ -26,12 +26,12 @@ architecture RTL of Uart_Tx is
     constant stop_bit  : std_ulogic := '1';
     signal tx_reg : std_ulogic := '1'; -- during idle state tx is high
     signal tx_reg_buffer : std_ulogic_vector(10 downto 0) := (others => '0');
-    signal tx_reg_buffer_sync : std_ulogic_vector(10 downto 0) := (others => '0');
-    signal count : integer := 0;
+    signal data_buffer : std_ulogic_vector(7 downto 0) := (others => '0');
+    shared variable count : integer := 0;
     signal tx_complete : std_ulogic := '0';
-
-
-
+    
+    type state is (IDLE,INITIAL,DATA,PARITY,LAST);
+    signal tx_state : state := IDLE;
 
 begin 
     DUT_CLK_BAUD : entity work.ClkDivider(RTL)
@@ -49,47 +49,62 @@ begin
             parity_out => parity_bit
         );
     
-    proc_tx : process(baud_clk,i_nrst_async) is
-        begin 
-        if (i_nrst_async = '0') then
+    proc_tx : process(baud_clk, i_nrst_async)
+        variable data_bit_count : integer := 0;
+    begin 
+        if (i_nrst_async = '0') then 
             tx_reg <= '1';
-            tx_reg_buffer <= (others => '0');
-        elsif (rising_edge(baud_clk)) then
-            if (parity_enable = '1') then
-                tx_reg_buffer <= start_bit & data_in & parity_bit & stop_bit;
-            else 
-                tx_reg_buffer <= start_bit & data_in & stop_bit & stop_bit; -- the stop bit is taken twice to maintain the width of the tx_reg_buffer
+            data_buffer <= (others => '0');
+            count := 0;
+            tx_state <= IDLE;
+            tx_complete <= '0';
+        elsif rising_edge(baud_clk) then 
+            
+            case tx_state is 
+                when IDLE =>
+                    tx_reg <= '1';
+                    data_buffer <= (others => '0');
+                    count := 0;
+                    tx_complete <= '0';
+                    if (start = '1') then 
+                        data_buffer <= data_in;
+                        tx_reg_buffer <= start_bit & data_in & stop_bit & stop_bit;
+                        tx_state <= INITIAL;
+                        count := count + 1;
+                    else 
+                        tx_state <= IDLE;
+                    end if;
+                when INITIAL =>
+                    tx_reg <= start_bit;
+                    count := count + 1;
+                    tx_state <= DATA;
+                    data_bit_count := 0;
+                    when DATA => 
+                    if data_bit_count < 8 then
+                        tx_reg <= data_buffer(data_bit_count);
+                        data_bit_count := data_bit_count + 1;
+                        count := count + 1;
+                    else
+                        data_bit_count := 0;
+                        if (parity_enable = '1') then
+                            tx_state <= PARITY;
+                            count := count + 1;
+                        else 
+                            tx_state <= LAST;
+                            count := count + 2;
+                        end if;
+                    end if;
+                when PARITY =>
+                            tx_reg <= parity_bit;
+                            count := count + 1;
+                            tx_state <= LAST;
+                when LAST =>
+                            tx_reg <= stop_bit;
+                            tx_complete <= '1';
+                            tx_state <= IDLE;
+            end case;
             end if;
-            if (start = '1') and (tx_complete = '1') then 
-                tx_complete <= '0';
-                tx_reg_buffer_sync <= tx_reg_buffer;
-            end if;
-            if (parity_enable = '1') then
-                if (count = 10) then 
-                    tx_reg <= tx_reg_buffer_sync(10);
-                    tx_complete <= '1';
-                    tx_reg_buffer_sync <= (others => '0');
-                    count <= 0;
-                else 
-                    tx_reg <= tx_reg_buffer_sync(10);
-                    tx_reg_buffer_sync <= tx_reg_buffer_sync(9 downto 0) & '0';
-                    count <= count + 1;
-                end if;
-            else 
-                if (count = 9) then 
-                    tx_reg <= tx_reg_buffer_sync(10);
-                    tx_complete <= '1';
-                    tx_reg_buffer_sync <= (others => '0');
-                    count <= 0;
-                else 
-                    tx_reg <= tx_reg_buffer_sync(10);
-                    tx_reg_buffer_sync <= tx_reg_buffer_sync(9 downto 0) & '0';
-                    count <= count + 1;
-                end if;
-            end if;
-        end if;
     end process proc_tx;
     tx <= tx_reg;
     ready <= tx_complete;
-
 end architecture RTL;
--- src/trytx.vhd
+++ src/trytx.vhd
@@ -96,3 +96,49 @@ begin
     tx <= tx_reg;
     ready <= tx_done;
 end architecture RTL;
+
+
+proc_tx : process(baud_clk,i_nrst_async) is
+    begin 
+    if (i_nrst_async = '0') then
+        tx_reg <= '1';
+        tx_reg_buffer <= (others => '0');
+    elsif (rising_edge(baud_clk)) then
+        if (parity_enable = '1') then
+            tx_reg_buffer <= start_bit & data_in & parity_bit & stop_bit;
+        else 
+            tx_reg_buffer <= start_bit & data_in & stop_bit & stop_bit; -- the stop bit is taken twice to maintain the width of the tx_reg_buffer
+        end if;
+        if (start = '1') and (tx_complete = '1') then 
+            tx_complete <= '0';
+            tx_reg_buffer_sync <= tx_reg_buffer;
+        end if;
+        if (parity_enable = '1') then
+            if (count = 10) then 
+                tx_reg <= tx_reg_buffer_sync(10);
+                tx_complete <= '1';
+                tx_reg_buffer_sync <= (others => '0');
+                count <= 0;
+            else 
+                tx_reg <= tx_reg_buffer_sync(10);
+                tx_reg_buffer_sync <= tx_reg_buffer_sync(9 downto 0) & '0';
+                count <= count + 1;
+            end if;
+        else 
+            if (count = 9) then 
+                tx_reg <= tx_reg_buffer_sync(10);
+                tx_complete <= '1';
+                tx_reg_buffer_sync <= (others => '0');
+                count <= 0;
+            else 
+                tx_reg <= tx_reg_buffer_sync(10);
+                tx_reg_buffer_sync <= tx_reg_buffer_sync(9 downto 0) & '0';
+                count <= count + 1;
+            end if;
+        end if;
+    end if;
+end process proc_tx;
+tx <= tx_reg;
+ready <= tx_complete;
+
+end architecture RTL;
