15c0ec8aca67e73df1d32aa39810bdc2108d132f
Varun Posimsetty <VarunSrinivas.Posimsetty@infineon.com>
2025-01-10 13:22:31 +0100

    still issue with output of rx

--- sim/tb_Uart.vhd
+++ sim/tb_Uart.vhd
@@ -17,6 +17,13 @@ architecture bhv of tb is
     signal start : std_ulogic := '0';
     signal tx : std_ulogic;
     signal ready : std_ulogic := '0';
+    signal rx : std_ulogic := '0';
+    signal data_out : std_ulogic_vector(7 downto 0) := (others => '0');
+    signal data_ready : std_ulogic := '0';
+    signal parity_error : std_ulogic := '0';
+    signal framing_error : std_ulogic := '1';
+
+
 
     begin 
 
@@ -45,6 +52,19 @@ architecture bhv of tb is
             parity_mode => parity_mode,
             tx => tx,
             ready => ready
+        );
+
+    DUT_RX : entity work.Uart_Rx(RTL)
+        port map(
+            i_clk_100MHz => clk,
+            i_nrst_async => rst,
+            rx  => rx,
+            parity_enable => parity_enable,
+            parity_mode => parity_mode,
+            data_out => data_out,
+            data_ready => data_ready,
+            parity_error => parity_error,
+            framing_error => framing_error
         );
 
         proc_clock_gen : process is
@@ -92,4 +112,10 @@ architecture bhv of tb is
                     rst <= '1';
                     wait;
         end process proc_tb;
+
+        proc_rx : process is 
+            begin 
+                wait for 10000 ns;
+                rx <= not rx;
+            end process proc_rx;
 end architecture bhv;
--- src/Uart_Rx.vhd
+++ src/Uart_Rx.vhd
@@ -22,6 +22,14 @@ architecture RTL of Uart_Rx is
     signal baud_clk : std_ulogic := '0';
     -- signals for parity
     signal parity_bit : std_ulogic := '0';
+    signal error_parity : std_ulogic := '1';
+    -- signals for UART rx 
+    signal rx_reg_buffer : std_ulogic_vector(10 downto 0) := (others => '0');
+    signal data_buffer : std_ulogic_vector(7 downto 0) := (others => '0');
+    signal rx_word : std_ulogic_vector(7 downto 0) := (others => '0');
+    
+    
+
 
 
 begin 
@@ -34,7 +42,7 @@ begin
 
     DUT_Parity : entity work.ParityLogic(RTL)
         port map(
-            data_in => data_in,
+            data_in => rx_reg_buffer(9 downto 2),
             parity_enable => parity_enable,
             parity_mode => parity_mode,
             parity_out => parity_bit
@@ -43,16 +51,47 @@ begin
     proc_rx : process(i_clk_100MHz,i_nrst_async) is 
         begin 
             if (i_nrst_async = '0') then 
+                rx_reg_buffer <= (others => '0');
+                data_buffer <= (others => '0');
                 
-
-
-
-
-
-
-
-
-
+            elsif (rising_edge(baud_clk)) then 
+                rx_reg_buffer <= rx_reg_buffer(9 downto 0) & rx;
+                if (rx_reg_buffer(10) = '0') and (rx_reg_buffer(0) = '1') then
+                    rx_word <= rx_reg_buffer(9 downto 2);
+                    framing_error <= '0';
+                    if (parity_enable = '1') then
+                        if (parity_mode = '0') then
+                            if (parity_bit = rx_reg_buffer(1)) then 
+                                data_buffer <= rx_word;
+                                error_parity <= '0';  
+                            else 
+                                error_parity <= '1';              
+                            end if;
+                        elsif (parity_mode = '1') then 
+                            if (parity_bit = rx_reg_buffer(1)) then 
+                                data_buffer <= rx_word;
+                                error_parity <= '0';
+                            else 
+                                error_parity <= '1';
+                            end if;   
+                        end if; 
+                    elsif(parity_enable = '0') then 
+                        rx_word <= rx_reg_buffer(9 downto 2);
+                        if (rx_reg_buffer(1) = '1' and rx_reg_buffer(0) = '1') then 
+                            data_buffer <= rx_word;
+                            parity_error <= '0';
+                        else 
+                            parity_error <= '1';
+                        end if;
+                    end if; 
+                    data_ready <= '1';
+                else 
+                    framing_error <= '1';
+                    data_ready <= '0';
+                end if;
+            end if;
+    end process proc_rx;
+    data_out <= data_buffer;
 end architecture RTL;
 
 
